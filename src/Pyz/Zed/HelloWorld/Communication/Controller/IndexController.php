21f651892bdc9cd10df872b57df9cc02ce51fc84
Sam-Burns <sam@sam-burns.com>
2017-07-13 11:45:11 +0100

    Minor stuff

--- src/Pyz/Zed/HelloWorld/Communication/Controller/IndexController.php
+++ src/Pyz/Zed/HelloWorld/Communication/Controller/IndexController.php
@@ -2,13 +2,15 @@
 
 namespace Pyz\Zed\HelloWorld\Communication\Controller;
 
-use Generated\Shared\Transfer\HelloWorldTransfer;
 use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+use Pyz\Zed\HelloWorld\Business\HelloWorldFacade;
+use Pyz\Zed\HelloWorld\Communication\HelloWorldCommunicationFactory;
+use Pyz\Zed\HelloWorld\Persistence\HelloWorldQueryContainer;
 
 /**
- * @method \Pyz\Zed\HelloWorld\Business\HelloWorldFacade getFacade()
- * @method \Pyz\Zed\HelloWorld\Communication\HelloWorldCommunicationFactory getFactory()
- * @method \Pyz\Zed\HelloWorld\Persistence\HelloWorldQueryContainer getQueryContainer()
+ * @method HelloWorldFacade getFacade()
+ * @method HelloWorldCommunicationFactory getFactory()
+ * @method HelloWorldQueryContainer getQueryContainer()
  */
 class IndexController extends AbstractController
 {
@@ -25,9 +27,7 @@ class IndexController extends AbstractController
     public function listAction()
     {
         $businessFacade = $this->getFacade();
-
         $peopleGreeted = $businessFacade->getEveryoneGreeted();
-
         return $this->viewResponse([
             'list' => var_export($peopleGreeted, true)
         ]);
